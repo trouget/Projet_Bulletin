772507ae7eff617a3004e4515275b51a096c40f9
Théo ROUGET <trouget@groupemontroland.fr>
2021-01-14 09:08:28 +0100

    Interface accueil

---
+++ Projet_Bulletin/Accueil.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Projet_Bulletin
+{
+    partial class Accueil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eleves_dtgv = new System.Windows.Forms.DataGridView();
+            this.nom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.prenom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.année1 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.année2 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.certification = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.eleves_dtgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // eleves_dtgv
+            // 
+            this.eleves_dtgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.eleves_dtgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nom,
+            this.prenom,
+            this.année1,
+            this.année2,
+            this.certification});
+            this.eleves_dtgv.Location = new System.Drawing.Point(129, 27);
+            this.eleves_dtgv.Name = "eleves_dtgv";
+            this.eleves_dtgv.Size = new System.Drawing.Size(544, 377);
+            this.eleves_dtgv.TabIndex = 0;
+            this.eleves_dtgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // nom
+            // 
+            this.nom.HeaderText = "nom";
+            this.nom.Name = "nom";
+            // 
+            // prenom
+            // 
+            this.prenom.HeaderText = "prenom";
+            this.prenom.Name = "prenom";
+            // 
+            // année1
+            // 
+            this.année1.HeaderText = "année1";
+            this.année1.Name = "année1";
+            this.année1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.année1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // année2
+            // 
+            this.année2.HeaderText = "année2";
+            this.année2.Name = "année2";
+            // 
+            // certification
+            // 
+            this.certification.HeaderText = "certification";
+            this.certification.Name = "certification";
+            // 
+            // Accueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.eleves_dtgv);
+            this.Name = "Accueil";
+            this.Text = "Accueil";
+            ((System.ComponentModel.ISupportInitialize)(this.eleves_dtgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView eleves_dtgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn prenom;
+        private System.Windows.Forms.DataGridViewButtonColumn année1;
+        private System.Windows.Forms.DataGridViewButtonColumn année2;
+        private System.Windows.Forms.DataGridViewButtonColumn certification;
+    }
+}
--- Projet_Bulletin/gen_certif.Designer.cs
+++ Projet_Bulletin/gen_certif.Designer.cs
@@ -40,94 +40,106 @@
             this.lbl_soutenance = new System.Windows.Forms.Label();
             this.btn_valider = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_retour = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // tbx_projet
             // 
-            this.tbx_projet.Location = new System.Drawing.Point(375, 329);
+            this.tbx_projet.Location = new System.Drawing.Point(281, 267);
+            this.tbx_projet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbx_projet.Name = "tbx_projet";
-            this.tbx_projet.Size = new System.Drawing.Size(100, 22);
+            this.tbx_projet.Size = new System.Drawing.Size(76, 20);
             this.tbx_projet.TabIndex = 68;
             // 
             // tbx_maths
             // 
-            this.tbx_maths.Location = new System.Drawing.Point(375, 299);
+            this.tbx_maths.Location = new System.Drawing.Point(281, 243);
+            this.tbx_maths.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbx_maths.Name = "tbx_maths";
-            this.tbx_maths.Size = new System.Drawing.Size(100, 22);
+            this.tbx_maths.Size = new System.Drawing.Size(76, 20);
             this.tbx_maths.TabIndex = 67;
             // 
             // tbx_livret
             // 
-            this.tbx_livret.Location = new System.Drawing.Point(375, 269);
+            this.tbx_livret.Location = new System.Drawing.Point(281, 219);
+            this.tbx_livret.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbx_livret.Name = "tbx_livret";
-            this.tbx_livret.Size = new System.Drawing.Size(100, 22);
+            this.tbx_livret.Size = new System.Drawing.Size(76, 20);
             this.tbx_livret.TabIndex = 66;
             // 
             // tbx_droit
             // 
-            this.tbx_droit.Location = new System.Drawing.Point(375, 241);
+            this.tbx_droit.Location = new System.Drawing.Point(281, 196);
+            this.tbx_droit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbx_droit.Name = "tbx_droit";
-            this.tbx_droit.Size = new System.Drawing.Size(100, 22);
+            this.tbx_droit.Size = new System.Drawing.Size(76, 20);
             this.tbx_droit.TabIndex = 65;
             // 
             // tbx_anglais
             // 
-            this.tbx_anglais.Location = new System.Drawing.Point(375, 213);
+            this.tbx_anglais.Location = new System.Drawing.Point(281, 173);
+            this.tbx_anglais.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbx_anglais.Name = "tbx_anglais";
-            this.tbx_anglais.Size = new System.Drawing.Size(100, 22);
+            this.tbx_anglais.Size = new System.Drawing.Size(76, 20);
             this.tbx_anglais.TabIndex = 64;
             // 
             // lbl_evalEtp
             // 
             this.lbl_evalEtp.AutoSize = true;
-            this.lbl_evalEtp.Location = new System.Drawing.Point(107, 272);
+            this.lbl_evalEtp.Location = new System.Drawing.Point(80, 221);
+            this.lbl_evalEtp.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_evalEtp.Name = "lbl_evalEtp";
-            this.lbl_evalEtp.Size = new System.Drawing.Size(143, 17);
+            this.lbl_evalEtp.Size = new System.Drawing.Size(107, 13);
             this.lbl_evalEtp.TabIndex = 63;
             this.lbl_evalEtp.Text = "Evaluation Entreprise";
             // 
             // lbl_notes1ere
             // 
             this.lbl_notes1ere.AutoSize = true;
-            this.lbl_notes1ere.Location = new System.Drawing.Point(107, 302);
+            this.lbl_notes1ere.Location = new System.Drawing.Point(80, 245);
+            this.lbl_notes1ere.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_notes1ere.Name = "lbl_notes1ere";
-            this.lbl_notes1ere.Size = new System.Drawing.Size(82, 17);
+            this.lbl_notes1ere.Size = new System.Drawing.Size(62, 13);
             this.lbl_notes1ere.TabIndex = 62;
             this.lbl_notes1ere.Text = "1ère Année";
             // 
             // lbl_projPedago
             // 
             this.lbl_projPedago.AutoSize = true;
-            this.lbl_projPedago.Location = new System.Drawing.Point(107, 244);
+            this.lbl_projPedago.Location = new System.Drawing.Point(80, 198);
+            this.lbl_projPedago.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_projPedago.Name = "lbl_projPedago";
-            this.lbl_projPedago.Size = new System.Drawing.Size(133, 17);
+            this.lbl_projPedago.Size = new System.Drawing.Size(100, 13);
             this.lbl_projPedago.TabIndex = 61;
             this.lbl_projPedago.Text = "Projet Pédagogique";
             // 
             // lbl_notes2eme
             // 
             this.lbl_notes2eme.AutoSize = true;
-            this.lbl_notes2eme.Location = new System.Drawing.Point(107, 332);
+            this.lbl_notes2eme.Location = new System.Drawing.Point(80, 270);
+            this.lbl_notes2eme.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_notes2eme.Name = "lbl_notes2eme";
-            this.lbl_notes2eme.Size = new System.Drawing.Size(92, 17);
+            this.lbl_notes2eme.Size = new System.Drawing.Size(70, 13);
             this.lbl_notes2eme.TabIndex = 60;
             this.lbl_notes2eme.Text = "2 ème Année";
             // 
             // lbl_soutenance
             // 
             this.lbl_soutenance.AutoSize = true;
-            this.lbl_soutenance.Location = new System.Drawing.Point(107, 216);
+            this.lbl_soutenance.Location = new System.Drawing.Point(80, 176);
+            this.lbl_soutenance.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_soutenance.Name = "lbl_soutenance";
-            this.lbl_soutenance.Size = new System.Drawing.Size(123, 17);
+            this.lbl_soutenance.Size = new System.Drawing.Size(93, 13);
             this.lbl_soutenance.TabIndex = 59;
             this.lbl_soutenance.Text = "Soutenance Orale";
             // 
             // btn_valider
             // 
-            this.btn_valider.Location = new System.Drawing.Point(375, 478);
+            this.btn_valider.Location = new System.Drawing.Point(281, 388);
+            this.btn_valider.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_valider.Name = "btn_valider";
-            this.btn_valider.Size = new System.Drawing.Size(100, 23);
+            this.btn_valider.Size = new System.Drawing.Size(75, 19);
             this.btn_valider.TabIndex = 69;
             this.btn_valider.Text = "Valider";
             this.btn_valider.UseVisualStyleBackColor = true;
@@ -135,18 +147,30 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Projet_Bulletin.Properties.Resources.certif;
-            this.pictureBox1.Location = new System.Drawing.Point(582, 213);
+            this.pictureBox1.Location = new System.Drawing.Point(436, 173);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(386, 106);
+            this.pictureBox1.Size = new System.Drawing.Size(477, 262);
             this.pictureBox1.TabIndex = 70;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // btn_retour
+            // 
+            this.btn_retour.Location = new System.Drawing.Point(83, 388);
+            this.btn_retour.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_retour.Name = "btn_retour";
+            this.btn_retour.Size = new System.Drawing.Size(75, 19);
+            this.btn_retour.TabIndex = 71;
+            this.btn_retour.Text = "Retour";
+            this.btn_retour.UseVisualStyleBackColor = true;
+            // 
             // gen_certif
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1043, 731);
+            this.ClientSize = new System.Drawing.Size(1047, 660);
+            this.Controls.Add(this.btn_retour);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_valider);
             this.Controls.Add(this.tbx_projet);
@@ -159,6 +183,7 @@
             this.Controls.Add(this.lbl_projPedago);
             this.Controls.Add(this.lbl_notes2eme);
             this.Controls.Add(this.lbl_soutenance);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "gen_certif";
             this.Text = "gen_certif";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -181,5 +206,6 @@
         private System.Windows.Forms.Label lbl_soutenance;
         private System.Windows.Forms.Button btn_valider;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_retour;
     }
 }
