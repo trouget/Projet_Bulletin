f7abaf57e07d3d060fe43c5a914e4a90d9c898db
Emma <emma.diziere@gmail.com>
2021-01-14 09:05:21 +0100

    Classe annee1 annee2 MORC

---
+++ Projet_Bulletin/annee2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Bulletin
+{
+    class annee2
+    {
+        public int id_bulletin2 { get; set; }
+        public int Moyenne2 { get; set; }
+        public int Administration_reseau { get; set; }
+        public int Anglais_technique { get; set; }
+        public int Application_hybride { get; set; }
+        public int Droit_informatique { get; set; }
+        public int Infrastructure_et_scripting { get; set; }
+        public int JEE { get; set; }
+        public int Outils_Mathematiques { get; set; }
+        public int Securite { get; set; }
+        public int Suivi_de_projet { get; set; }
+        public int Systeme_open_source { get; set; }
+        public int Systeme_proprietaire { get; set; }
+        public int Technologie_web_dynamique { get; set; }
+        public int Deploiement_serveur { get; set; }
+        public int Big_data { get; set; }
+        public int Arduino { get; set; }
+        public int Projet { get; set; }
+        public int Livret_de_suivi { get; set; }
+
+        public annee2()
+        {
+
+        }
+    }
+}
